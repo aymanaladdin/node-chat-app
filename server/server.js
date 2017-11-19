0d1272f5e57e360c28ab6a46fbb59dced330fe97
Ayman_Aladdin <ayman.alaa1012@gmail.com>
2017-11-19 14:00:56 +0200

    init socket io 'connect' and 'disconnect' handlers

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
 const path = require("path");
+const http = require("http");
 
 const express = require("express");
+const socketIO = require("socket.io");
 
 const root = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000
-const app = express();
 
-app.use((req, res, next)=>{
-    console.log(req.method, req.url);
-    next();
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log(`new user connected`);
+
+    socket.on('disconnect', ()=>{
+        console.log('user disconnected');
+    })
 });
+
+
+// app.use((req, res, next)=>{
+//     console.log(req.method, req.url);
+//     next();
+// });
 
 app.use(express.static(root));
 
-app.listen(port, ()=> console.log(`app is running on port ${port}`));
+server.listen(port, ()=> console.log(`app is running on port ${port}`));
 
